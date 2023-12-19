72954f36c5e210df26a13c425549a567961b0874
MarioPalaciosLop <plopezmarioalfr@gmail.com>
2023-12-19 16:49:39 -0500

    first release

--- src/main/java/com/palacios/infraccionservice/service/InfraccionService.java
+++ src/main/java/com/palacios/infraccionservice/service/InfraccionService.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 
+import com.palacios.infraccionservice.entity.Infraccion;
+
 public interface InfraccionService {
-    
+
+    public List<Infraccion> findAll(Pageable page);
+
+    public List<Infraccion> findByNombre(String nombre, Pageable page);
+
+    public Infraccion findById(int id);
+
+    public Infraccion save(Infraccion empresa);
+
+    public Infraccion update(Infraccion empresa);
+
+    public void delete(int id);
+
 }
